1ebe9e248c3da11948fcffd9108765c50611c17f
NousheenRazzak <nousheen461@gmail.com>
2024-11-12 10:01:48 +0500

    my-resume

---
+++ app.ts
@@ -0,0 +1,14 @@
+// function clickMe() {
+//     let button = document.getElementById("myDiv") as HTMLButtonElement;
+//     if (button.style.display === "none") {
+//       button.style.display = "block";
+//     } else {
+//       button.style.display = "none";
+//     }
+//   }
+
+let icons = document.getElementsByClassName(".details");
+
+function clickMe(){
+    
+}
