c1a52d44f2a0feaafc594d98080ca954accfed5e
NousheenRazzak <nousheen461@gmail.com>
2024-11-14 10:41:26 +0500

    add functionality

---
+++ app.js
@@ -0,0 +1,72 @@
+function summary() {
+    var click = document.getElementById("summary-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function experience() {
+    var click = document.getElementById("experience-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function education() {
+    var click = document.getElementById("education-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function projects() {
+    var click = document.getElementById("projects-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function contact() {
+    var click = document.getElementById("contact-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function skills() {
+    var click = document.getElementById("skills-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function courses() {
+    var click = document.getElementById("courses-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+function hobbies() {
+    var click = document.getElementById("hobbies-click");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
---
+++ app.ts
@@ -0,0 +1,71 @@
+function summary() {
+    let click = document.getElementById("summary-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function experience() {
+    let click = document.getElementById("experience-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function education() {
+    let click = document.getElementById("education-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function projects() {
+    let click = document.getElementById("projects-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function contact() {
+    let click = document.getElementById("contact-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function skills() {
+    let click = document.getElementById("skills-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function courses() {
+    let click = document.getElementById("courses-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
+
+  function hobbies() {
+    let click = document.getElementById("hobbies-click") as HTMLHeadingElement;
+    if (click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+  }
